78827dc02c724d35dafa3dbda4b3f81edf52b903
wilsonckm <wilsonckmchan@gmail.com>
2023-11-16 14:26:09 -0500

    Added Values component

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import Value from "./src/components/Values";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Tesasdft</Text>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>HealthKit Testing</Text>
+      </View>
+      <View style={styles.values}>
+        <Value label={"Steps"} value={10} />
+        <Value label={"Distance"} value={100} />
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "#black",
+    // alignItems: "center",
     justifyContent: "center",
+  },
+
+  values: {
+    flexDirection: "row",
+    gap: 25,
+    flexWrap: "wrap",
   },
 });
